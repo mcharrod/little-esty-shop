7c4a3cbb7da278e9c33a35390362e2cc432f8966
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-23 12:33:05 -0700

    Feat: Admin_invoices testing and passing with new migration.

--- spec/features/admin/invoices/index_spec.rb
+++ spec/features/admin/invoices/index_spec.rb
@@ -7,10 +7,17 @@ RSpec.describe 'admin_invoices', type: :feature do
 
     @merchant1 = Merchant.create!(name: "The Tornado")
     @item1 = @merchant1.items.create!(name: "SmartPants", description: "IQ + 20", unit_price: 125)
+    @item2 = @merchant1.items.create!(name: "FunPants", description: "Cha + 20", unit_price: 2000)
+    @item3 = @merchant1.items.create!(name: "FitPants", description: "Con + 20", unit_price: 150)
     @customer1 = Customer.create!(first_name: "Marky", last_name: "Mark" )
-
+    @customer2 = Customer.create!(first_name: "Larky", last_name: "Lark" )
+    @customer3 = Customer.create!(first_name: "Sparky", last_name: "Spark" )
     @invoice1 = @customer1.invoices.create!(status: 1)
+    @invoice2 = @customer2.invoices.create!(status: 1)
+    @invoice3 = @customer2.invoices.create!(status: 1)
     @invoice_item1 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item1.id, quantity: 2, unit_price: 125, status: 1)
+    @invoice_item2 = InvoiceItem.create!(invoice_id: @invoice1.id, item_id: @item2.id, quantity: 2, unit_price: 2000, status: 2)
+    @invoice_item3 = InvoiceItem.create!(invoice_id: @invoice2.id, item_id: @item3.id, quantity: 5, unit_price: 125, status: 0)
   end
 
 
@@ -20,7 +27,7 @@ RSpec.describe 'admin_invoices', type: :feature do
     expect(page).to have_content("Invoice Number: #{@invoice1.id}")
     expect(page).to have_content("Invoice Number: #{@invoice2.id}")
     expect(page).to have_content("Invoice Number: #{@invoice3.id}")
-
+  end
   it "has a link to the invoice show page" do
     visit "/admin/invoices"
 
