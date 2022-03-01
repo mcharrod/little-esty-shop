b4f0685b159f3eb3a5188b2375b687206b487962
Joseph W Galvin <jwgalvin@gmail.com>
2022-02-28 19:55:24 -0700

    Fix Item model spec after model method tweaked.

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -40,13 +40,14 @@ RSpec.describe Item, type: :model do
 
     it  "best day with top five items setup" do
       merchant1 = Merchant.create!(name: "The Tornado")
-      item1 = merchant1.items.create!(name: "SmartPants", description: "IQ + 20", unit_price: 120)
-      item2 = merchant1.items.create!(name: "FunPants", description: "Cha + 20", unit_price: 2000)
-      item3 = merchant1.items.create!(name: "FitPants", description: "Con + 20", unit_price: 150)
-      item4 = merchant1.items.create!(name: "VeinyShorts", description: "Str + 20", unit_price: 1400)
-      item5 = merchant1.items.create!(name: "SpringSocks", description: "DX + 20", unit_price: 375)
-      item6 = merchant1.items.create!(name: "UnderRoos", description: "SNUG!", unit_price: 25)
-      item7 = merchant1.items.create!(name: "SunStoppers", description: "Eclipse ready!", unit_price: 50)
+      smart = merchant1.items.create!(name: "SmartPants", description: "IQ + 20", unit_price: 120)
+      fun = merchant1.items.create!(name: "FunPants", description: "Cha + 20", unit_price: 2000)
+      fit = merchant1.items.create!(name: "FitPants", description: "Con + 20", unit_price: 150)
+      veiny = merchant1.items.create!(name: "VeinyShorts", description: "Str + 20", unit_price: 1400)
+      socks = merchant1.items.create!(name: "SpringSocks", description: "DX + 20", unit_price: 375)
+      under = merchant1.items.create!(name: "UnderRoos", description: "SNUG!", unit_price: 25)
+      sun = merchant1.items.create!(name: "SunStoppers", description: "Eclipse ready!", unit_price: 50)
+
       customer1 = Customer.create!(first_name: "Marky", last_name: "Mark" )
       customer2 = Customer.create!(first_name: "Larky", last_name: "Lark" )
       customer3 = Customer.create!(first_name: "Sparky", last_name: "Spark" )
@@ -58,23 +59,35 @@ RSpec.describe Item, type: :model do
       invoice4 = customer4.invoices.create!(status: 2, created_at: "2014-05-22")
       invoice5 = customer4.invoices.create!(status: 2, created_at: "2014-05-19")
 
-      invoice_item2 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item1.id, quantity: 2, unit_price: 2000, status: 0)
-      invoice_item4 = InvoiceItem.create!(invoice_id: invoice2.id, item_id: item1.id, quantity: 2, unit_price: 120, status: 1)
-      invoice_item7 = InvoiceItem.create!(invoice_id: invoice3.id, item_id: item7.id, quantity: 15, unit_price: 50, status: 2)
-      invoice_item3 = InvoiceItem.create!(invoice_id: invoice5.id, item_id: item3.id, quantity: 5, unit_price: 125, status: 0)
-      invoice_item1 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item2.id, quantity: 2, unit_price: 125, status: 0)
-      invoice_item6 = InvoiceItem.create!(invoice_id: invoice4.id, item_id: item6.id, quantity: 20, unit_price: 25, status: 2)
-      invoice_item5 = InvoiceItem.create!(invoice_id: invoice3.id, item_id: item2.id, quantity: 2, unit_price: 2000, status: 1)
-      invoice_item8 = InvoiceItem.create!(invoice_id: invoice5.id, item_id: item2.id, quantity: 1, unit_price: 2000, status: 1)
-      invoice_item9 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: item4.id, quantity: 3, unit_price: 1400, status: 2)
 
-      transaction1 = Transaction.create!(credit_card_number: 123456, result: 1, invoice_id: invoice1.id)
-      transaction2 = Transaction.create!(credit_card_number: 123456, result: 1, invoice_id: invoice2.id)
-      transaction3 = Transaction.create!(credit_card_number: 123456, result: 1, invoice_id: invoice3.id)
-      transaction4 = Transaction.create!(credit_card_number: 123456, result: 1, invoice_id: invoice4.id)
-      transaction5 = Transaction.create!(credit_card_number: 123456, result: 1, invoice_id: invoice5.id)
-      expect(item1.best_day).to eq("Thursday, May 22, 2014")
-      expect(item2.best_day).to eq("Tuesday, May 20, 2014")
-      expect(item3.best_day).to eq("Monday, May 19, 2014")
+      # fun pants has 4500 revenue (500 x 5) + 1000 + 1000
+      invoice_item1 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: fun.id, quantity: 5, unit_price: 500, status: 0)
+      invoice_item5 = InvoiceItem.create!(invoice_id: invoice3.id, item_id: fun.id, quantity: 1, unit_price: 1000, status: 1)
+      invoice_item8 = InvoiceItem.create!(invoice_id: invoice5.id, item_id: fun.id, quantity: 1, unit_price: 1000, status: 1)
+
+      # smart pants has 4400 revenue (2000 x 2) + (100 x 4)
+      invoice_item2 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: smart.id, quantity: 2, unit_price: 2000, status: 0)
+      invoice_item4 = InvoiceItem.create!(invoice_id: invoice2.id, item_id: smart.id, quantity: 4, unit_price: 100, status: 1)
+
+      # veiny shorts has 1400 revenue
+      invoice_item9 = InvoiceItem.create!(invoice_id: invoice1.id, item_id: veiny.id, quantity: 3, unit_price: 1400, status: 2)
+
+      # sun stoppers has 700 revenue (100 x 7)
+      invoice_item7 = InvoiceItem.create!(invoice_id: invoice3.id, item_id: sun.id, quantity: 7, unit_price: 100, status: 2)
+
+      # fit pants has 500 revenue (100 x 5)
+      invoice_item3 = InvoiceItem.create!(invoice_id: invoice5.id, item_id: fit.id, quantity: 5, unit_price: 100, status: 0)
+
+      # under roos has 75 revenue (25 x 3)
+      invoice_item6 = InvoiceItem.create!(invoice_id: invoice4.id, item_id: under.id, quantity: 3, unit_price: 25, status: 2)
+
+      transaction1 = Transaction.create!(credit_card_number: 123456, result: 0, invoice_id: invoice1.id)
+      transaction2 = Transaction.create!(credit_card_number: 123456, result: 0, invoice_id: invoice2.id)
+      transaction3 = Transaction.create!(credit_card_number: 123456, result: 0, invoice_id: invoice3.id)
+      transaction4 = Transaction.create!(credit_card_number: 123456, result: 0, invoice_id: invoice4.id)
+      transaction5 = Transaction.create!(credit_card_number: 123456, result: 0, invoice_id: invoice5.id)
+      expect(smart.best_day).to eq(smart.created_at.strftime("Thursday, May 22, 2014"))
+      expect(fun.best_day).to eq(fun.created_at.strftime("Thursday, May 22, 2014"))
+      expect(fit.best_day).to eq(fit.created_at.strftime("Monday, May 19, 2014"))
     end
 end
