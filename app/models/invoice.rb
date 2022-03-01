5137e77f65898a0e7da7dc18e2fd82fb26710da4
Mark Ertmer <mark.ertmer@gmail.com>
2022-03-01 15:38:18 -0700

    Refactor: Controller, Model, Model Spec

--- app/models/invoice.rb
+++ app/models/invoice.rb
@@ -13,16 +13,14 @@ class Invoice < ApplicationRecord
     self.items.sum(:unit_price)
   end
 
-
   def format_date
     created_at.strftime("%A, %B %d, %Y")
   end
 
-  def self.all_open_oldest_first
-    where(status: "in progress").order(:created_at)
-  end
-
   def self.incomplete
-    Invoice.where.not(status: 1).joins(:invoice_items).where.not(status: 2).group("invoices.id")
+
+    # Invoice.where.not(status: 1).joins(:invoice_items).where.not(status: 2).group("invoices.id")
+    where(status: 0).order(:created_at)
+
   end
 end
